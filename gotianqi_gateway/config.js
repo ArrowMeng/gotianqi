13c89e76436bef67d0ee7b32ebf2475b00ace271
ArrMeng <qiangjun.meng@gmail.com>
2015-10-18 17:43:05 +0800

    cores issue

--- gotianqi_gateway/config.js
+++ gotianqi_gateway/config.js
@@ -3,7 +3,7 @@ module.exports = {
   serverPort: 8000,
   sessionCookieKey: "connect.sid",
   sessionSecret: "SESIORsecret",
-  allowedCORSOrigins: "http://127.0.0.1",   
+  allowedCORSOrigins: "http://127.0.0.1:8080",
   
   AUTH_SERVER: process.env.AUTH_SERVER || 'http://127.0.0.1:3000',
   DB_SERVER: process.env.DB_SERVER || 'http://127.0.0.1:3000',
