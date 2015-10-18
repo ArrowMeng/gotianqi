3997a10de3b86198c246dd80fe40e47f0a9338bc
ArrMeng <qiangjun.meng@gmail.com>
2015-10-18 20:44:39 +0800

    8080

--- gotianqi_gateway/config.js
+++ gotianqi_gateway/config.js
@@ -3,7 +3,7 @@ module.exports = {
   serverPort: 8000,
   sessionCookieKey: "connect.sid",
   sessionSecret: "SESIORsecret",
-  allowedCORSOrigins: "*",
+  allowedCORSOrigins: "http://locahost:8080",
   
   AUTH_SERVER: process.env.AUTH_SERVER || 'http://localhost:3000',
   DB_SERVER: process.env.DB_SERVER || 'http://localhost:3000',
