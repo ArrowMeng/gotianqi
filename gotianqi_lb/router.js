101aa52062d033bfe564d9561958aa2353db5bb3
ArrMeng <qiangjun.meng@gmail.com>
2015-10-27 21:57:50 +0800

    header

--- gotianqi_lb/router.js
+++ gotianqi_lb/router.js
@@ -11,6 +11,8 @@ bouncy(function (req, bounce) {
         stable : '0.0.x'
     }[domains[0]] || '0.0.x');
 
+    var res = bounce.respond();
+    
     res.header("Access-Control-Allow-Origin", config.allowedCORSOrigins);
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type");
@@ -19,7 +21,6 @@ bouncy(function (req, bounce) {
     var ps = server.query(service);
     
     if (ps.length === 0) {
-        var res = bounce.respond();
         res.end("service not available\n");
     }
     else {
